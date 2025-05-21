668526140dd7358cc25c0f8fcbdac88e01bd37d5
Grai Rudolf <rtech777r@gmail.com>
2025-05-21 23:19:51 +0100

    Replaced cards in course grids

--- src/components/CourseSeriesGrid.tsx
+++ src/components/CourseSeriesGrid.tsx
@@ -25,7 +25,7 @@ const courses = [
     status: "locked",
     progress: 0,
     image:
-      "https://images.unsplash.com/photo-1532012197267-da84d127e765?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+      "public/uploads/bcc_dis.jpeg",
     free: false,
   },
   {
@@ -36,7 +36,7 @@ const courses = [
     status: "locked",
     progress: 0,
     image:
-      "https://images.unsplash.com/photo-1476275466078-4007374efbbe?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80",
+      "public/uploads/bcc_sanct.jpg",
     free: false,
   },
   {
